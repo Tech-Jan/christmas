1178f5446fff51cdd902c8dbc6c4ff99fb4907b1
tech-jan <trewej@hotmail.com>
2022-12-04 10:33:41 +0100

    cleaned up day 4

--- day4.py
+++ day4.py
@@ -3,30 +3,32 @@ f = open("input4", "r").read()
 pair_elves = f.split("\n")
 print(pair_elves)
 
+
 def transform_range(read_elve):
     """Returns list [0] =startrange [1] = endrange"""
     elve_work = []
     elverange = read_elve.split("-")
-    for i in range (0,100):
-        if i < int(elverange[0]) or i > int(elverange[1]):
+    for b in range(0, 100):
+        if b < int(elverange[0]) or b > int(elverange[1]):
             elve_work.append(".")
         else:
-            elve_work.append(i)
-    return  set(filter(lambda filt: filt !=".",elve_work))
+            elve_work.append(b)
+    return set(filter(lambda filt: filt != ".", elve_work))
 
 
-def overlap(range1, range2):
-   if range1.intersection(range2):
-       return 1
-   else:
-       return 0
+def overlap(set1, set2):
+    """uses sets to compare"""
+    if set1.intersection(set2):
+        return 1
+    else:
+        return 0
 
 
 def transform_range2(read_elve):
     """Returns list [0] =startrange [1] = endrange. works wiuth overlap2"""
     elve_work = []
     elverange = read_elve.split("-")
-    for i in range (0,100):
+    for i in range(0, 100):
         if i < int(elverange[0]) or i > int(elverange[1]):
             elve_work.append(".")
         else:
@@ -38,7 +40,7 @@ def overlap2(range1, range2):
     """overlap variant 2. works with transform_range2 variant 2"""
     for i in range1:
         for b in range2:
-            if i==b and i!=".":
+            if i == b and i != ".":
                 return 1
     return 0
 
@@ -48,28 +50,26 @@ def read_range(read_elve):
     elverange = read_elve.split("-")
     elvestart = int(elverange[0])
     elveend = int(elverange[1])
-    elve_range_int=[elvestart, elveend]
+    elve_range_int = [elvestart, elveend]
     return elve_range_int
+
 
 def compare_range(range1, range2):
     if range1[0] <= range2[0] and range1[1] >= range2[1]:
-        print("this is entirly contained by elve numero 1")
+        print("elve2 entirly contained in elve1")
         return 1
     elif range2[0] <= range1[0] and range2[1] >= range1[1]:
-        print("this is entirly contained by elve numero 2")
+        print("elve1 entirly contained in elve2")
         return 1
     else:
         print("maybe lot of work for each")
         return 0
 
 
-
-
-
 sum_fully_contains = 0
 sum_overlap = 0
 sum_overlap2 = 0
-i=0
+
 for elve in pair_elves:
     elve = elve.split(",")
     elve1_range = read_range(elve[0])
@@ -78,11 +78,10 @@ for elve in pair_elves:
     elve2_longrange = transform_range((elve[1]))
     elve1_longrange2 = transform_range((elve[0]))
     elve2_longrange2 = transform_range((elve[1]))
+    print(elve)
     sum_fully_contains += compare_range(elve1_range, elve2_range)
     sum_overlap += overlap(elve1_longrange, elve2_longrange)
     sum_overlap2 += overlap2(elve1_longrange2, elve2_longrange2)
-    print(sum_fully_contains)
-    print(sum_overlap)
-    print(sum_overlap2)
-
-
+print(sum_fully_contains)
+print(sum_overlap)
+print(sum_overlap2)
